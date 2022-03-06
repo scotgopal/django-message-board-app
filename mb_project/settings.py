af642b4e37aa6dbd3bbe962a338ed8a2bff7fe6a
Scotrraaj <scotgopal@gmail.com>
2022-03-06 23:51:00 +0800

    Prep for Heroku Deployment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "t$w-)++iwvgg8j8naoy21kr5%&a%sh*@6rlr4_!@2fv@6&kc-q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
